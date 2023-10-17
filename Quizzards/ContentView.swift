20a6566522535b31a5134c70952fce87c1add9e7
Joan Nyambura <njugunajoan35@gmail.com>
2023-10-17 18:20:48 +0300

    Initial Commit

---
+++ Quizzards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quizzards
+//
+//  Created by Jojo on 17/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Quizzards/QuizzardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzardsApp.swift
+//  Quizzards
+//
+//  Created by Jojo on 17/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizzardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
